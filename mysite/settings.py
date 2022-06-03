054165b90028207dd99dfd6f65a2fa614127bf16
zyzCodes <diego.earanas1@gmail.com>
2022-06-03 09:34:25 -0600

    security commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@gqg$^$j#p9_e*h3wa5$-a-=3c3m1mjv!x^04!14h%9hq-q(o!'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
